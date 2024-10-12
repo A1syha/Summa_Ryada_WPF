4db441931313d02277a451518c9634cb9b3a1b6e
dima- <dima-@VOROBEY>
2024-10-12 19:47:24 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _316a_Vorobiev_Dmitriy_Variant_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ochistit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Chislo_X.Text = "";
+            Tochnost.Text = "";
+            Label4.Content = "";
+        }
+
+        private void Vipolnit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(Tochnost.Text, out double epsilon) && double.TryParse(Chislo_X.Text, out double x) && x != 0 && epsilon > 0)
+            {
+                double result = Raschet(x, epsilon);
+                Label4.Content = result.ToString();
+            }
+            else
+            {
+                Label4.Content = "Ошибка ввода!";
+            }
+        }
+        private double Raschet(double x, double epsilon)
+        {
+            StreamWriter writer = new StreamWriter("PROMEJUTOCHNIE_RACHETI.txt");
+            writer.WriteLine("Итерация\tРезультат\t\tСумма");
+            double sum_result = 0;
+            double result;
+            int k = 0;
+            do
+            {
+                double chislitel = Math.Pow(-1, k) * Math.Pow(x, 4 * k + 3);
+                double znamenatel = Factorial(2 * k + 1) * (4 * k + 3);
+                result = chislitel / znamenatel;
+                sum_result += result;
+                k++;
+                writer.WriteLine($"{k,-10}\t\t{result,-20}\t{sum_result,-20}");
+            } while (Math.Abs(result) >= epsilon);
+            writer.Close();
+            return sum_result;
+        }
+
+        public double Factorial(int n)
+        {
+            double result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        private void Raschet_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = "PROMEJUTOCHNIE_RACHETI.txt";
+            Window1 window1 = new Window1(filePath);
+            window1.Show();
+        }
+
+        private void Otdelniy_Potok_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(Tochnost.Text, out double epsilon) && double.TryParse(Chislo_X.Text, out double x) && x != 0 && epsilon > 0)
+            {
+                Window2 window2 = new Window2(x, epsilon);
+                window2.Show();
+            }
+            else
+            {
+                Label4.Content = "Ошибка ввода!";
+            }
+        }
+    }
+}
---
+++ Window2.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+
+namespace _316a_Vorobiev_Dmitriy_Variant_2
+{
+    public partial class Window2 : Window
+    {
+        private BackgroundWorker worker;
+        private double x;
+        private double epsilon;
+
+        public Window2(double x, double epsilon)
+        {
+            InitializeComponent();
+            this.x = x;
+            this.epsilon = epsilon;
+            worker = new BackgroundWorker();
+            worker.WorkerSupportsCancellation = true;
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += DoWork;
+            worker.ProgressChanged += ProgressChanged;
+            worker.RunWorkerCompleted += Completed;
+            worker.RunWorkerAsync();
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            double sum_result = 0;
+            double result;
+            int k = 0;
+            do
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true; 
+                    return;
+                }
+                double chislitel = Math.Pow(-1, k) * Math.Pow(x, 4 * k + 3);
+                double znamenatel = Factorial(2 * k + 1) * (4 * k + 3);
+                result = chislitel / znamenatel;
+                sum_result += result;
+                worker.ReportProgress(k, new { Iteration = k + 1, Result = result, Sum = sum_result });
+                k++;
+                System.Threading.Thread.Sleep(100);
+            } while (Math.Abs(result) >= epsilon);
+            e.Result = sum_result;
+        }
+
+        private void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            var data = (dynamic)e.UserState;
+            table.AppendText($"Итерация: {data.Iteration}, Результат: {data.Result}, Сумма: {data.Sum}\n");
+
+            if (table.Visibility == Visibility.Visible)
+            {
+                table.ScrollToEnd();
+            }
+        }
+
+        private void Completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                table.AppendText("Операция прервана пользователем\n");
+            }
+            else
+            {
+                table.AppendText($"Операция выполнена, итоговый результат = {e.Result}\n");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            worker.CancelAsync();
+        }
+
+
+        public double Factorial(int n)
+        {
+            double result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
